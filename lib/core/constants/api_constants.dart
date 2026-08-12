ad0ab15738d3234014373d52ee012f10b2efce36
truongdq <truongdq@seatechit.com.vn>
2026-08-12 16:24:48 +0700

    chore: connect app to production api

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = 'http://10.0.2.2:8080';
+  static const String baseUrl = 'https://event.thongtinchinhhieu.site';
   static const String apiPrefix = '/api/v1';
 
   // Auth
